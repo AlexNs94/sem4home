0d4e5cf3c4f162a64e475b9fa36f54a3b02bf3d4
alexns <alexns9419@yandex.ru>
2022-12-22 18:36:55 +0500

    zadacha27

---
+++ zadcha27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+int DataInput(string str)
+{
+Console.WriteLine(str);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+int SumNumbers(int num) 
+{ 
+    int result = 0; 
+    while(num > 0) 
+    { 
+        result += num % 10; 
+        num = num / 10;         
+    } 
+    return result; 
+} 
+ 
+Console.Clear(); 
+ 
+int number = DataInput("Введите число "); 
+int res = SumNumbers(number); 
+ 
+Console.WriteLine($"{number} -> {res}");
